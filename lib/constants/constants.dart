6795b95fd6f3d8b2093f4ddfa8dc860163016403
Matin <matin7najafi@gmail.com>
2025-08-23 11:56:49 +0330

    added constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color blackColor = Color(0xff080808);
+const Color greyColor = Color(0xff171719);
+const Color darkWhiteColor = Colors.white54;
+const Color whiteColor = Color(0xffFFFFFF);
+const Color orangeColor = Color(0xffF09A00);
+const String developrName = 'Mtiw_dev';
+const String appName = 'Calculator';
+const String appVersion = '1.0.0';
+const String appDescription = 'A simple calculator app';
